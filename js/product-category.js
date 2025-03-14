1ef9cc88539d2a82a213226336d843bbda77b2b1
Hasindu Sankalpa <ubayawickrama7@gmail.com>
2025-03-14 13:37:41 +0530

    added categorization

---
+++ js/product-category.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const category = urlParams.get("category");
+    loadProducts(category);
+});
+
+function loadProducts(category) {
+    const productContainer = document.getElementById("product-container");
+
+    if (!products[category]) {
+        productContainer.innerHTML = "<p>No products found in this category.</p>";
+        return;
+    }
+
+    productContainer.innerHTML = "";
+
+    products[category].forEach(product => {
+        const productElement = document.createElement("div");
+        productElement.classList.add("product");
+
+        productElement.innerHTML = `
+            <a href="product-details.html?id=${product.id}">
+                <img src="${product.images[0]}" alt="${product.title}">
+                <h3>${product.title}</h3>
+                <p>Starting from Rs.${product.basePrice.toLocaleString()}</p>
+            </a>
+        `;
+
+        productContainer.appendChild(productElement);
+    });
+}
---
+++ js/product-data.js
@@ -0,0 +1,47 @@
+const products = {
+    "gaming-laptops": [
+        {
+            id: "tuf-f15",
+            title: "ASUS TUF Gaming F15 RTX 4070",
+            images: ["images/tuf_f15_1.png", "images/tuf_f15_2.png"],
+            basePrice: 409000,
+            category: "gaming-laptops",
+            brand:"Asus"
+        },
+        {
+            id: "rog-g16",
+            title: "ASUS ROG Strix G16 RTX 4070",
+            images: ["images/rog_g16_1.png", "images/rog_g16_2.png"],
+            basePrice: 520000,
+            category: "gaming-laptops",
+            brand:"Asus"
+        },
+        {
+            id: "legion-5",
+            title: "Lenovo Legion 5 RTX 4060",
+            images: ["images/legion_5_1.png", "images/legion_5_2.png"],
+            basePrice: 375000,
+            category: "gmaing-laptops",
+            brand:"Lenovo"
+        },
+        {
+            id: "katana-15",
+            title: "MSI Katana 15 I7 13TH GEN RTX 4060 8GB",
+            images: ["images/legion_5_1.png", "images/legion_5_2.png"],
+            basePrice: 375000,
+            category: "gaming-laptops",
+            brand:"MSI"
+        }
+    ],
+    
+    "graphics-cards": [
+        {
+            id: "tuf-rtx4090",
+            title: "ASUS TUF Gaming RTX 4090",
+            images: ["images/tuf_4090_1.png", "images/tuf_4090_2.png"],
+            basePrice: 620000,
+            category: "graphics-cards",
+            brand:"Asus"
+        }
+    ]
+};
